3b908d9412b755e5cf70dc69737dfc4e08a8c87d
chengcheng <chengcheng@haoqitech.com.cn>
2018-01-23 18:55:44 +0800

    代码提交

---
+++ 03-router/router/app/lion2.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class Lion extends React.Component{
+  render(){
+    return (
+      <div>B页面</div>
+    )
+  }
+}
---
+++ 03-router/router/app/lion3.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class Lion extends React.Component{
+  render(){
+    return (
+      <div>C页面</div>
+    )
+  }
+}
